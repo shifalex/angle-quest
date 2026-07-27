b6306ecc891bbe37d513e0be1ffdc17c34b1e770
shifalex <90042959+shifalex@users.noreply.github.com>
2026-07-27 22:06:01 +0300

    Build site with supported vinext runtime

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main>
+      <iframe
+        src="/legacy/index.html"
+        title="Angle Quest"
+        allow="autoplay"
+      />
+    </main>
+  );
+}
